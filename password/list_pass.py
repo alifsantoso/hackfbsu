511ea05d27b3c531b84dccada2c03b6ba2cf79dd
alifsantoso <86616463+alifsantoso@users.noreply.github.com>
2021-07-10 23:35:11 +0700

    Add files via upload

---
+++ password/list_pass.py
@@ -0,0 +1,23 @@
+# Silahkan Jika Ingin Mengubah Listpass Nya
+# Jika Ingin Mengubah pw_tambahan Harap Gunakan Pemisah Menggunakan |
+pw_tambahan="""
+
+bismillah|sayang|bangsat|anjing|kontol|mantan|kesayangan|sayangku|12desember2000|12122000|12desember2005|12122005|anjingbanget|mantanku|sayangbanget|sayangnya|19februari2002|cantik|cantiknya|apakabar|akucantik|kopihitam|kopitubrukcapgajah|makanbakso|pisanggoreng|pisang|setiahati|shterate|cucueyangsuro|dewa|angklingdarma|anakfreefire|playerff|123456789|987654321|1234567890|0987654321|keluargabesar|mobaplayer|bocilff|password@|kangen|akusayangkamu|cintaku|cintakupadamu|anakff|cinta|akusayang|password|anakrantau|anakmoba|anakmobilelegends|duaribudua|2021|nontontv|nonton|assalamualaikum|iribilangbos|ganteng|akusayangbanget|rebahan|kontolgede|ngacengterus|ngaceng|sange|sangne|ngentot|ngentodanjing|mabarfreefire|adayangbaru|jedagjedug|mengharapkanmu|mengharapkan|bucinsquad|bucins|bokep|kontolgedi|luwakkopi|senja|maingitar|tiktokers|tiktok|demeises|janjikupadamu|bintanghati|bintanghatiku|menjagamu|qwerty|qwertyuiop|nungguinya|goodnight|cintadanluka|manisbuahkelapa|menggombal|kasihku|galau|lagugalau|indahpadawaktunya|stw|jancok|pencaricinta|sejati|pencaricintasejati|psht|pshtjaya|akuakanmenjagamu|tutikstw|citraayuningtyas|gemilang|geminibintang|gemini|taurus|mendesah|lontee|lonte|jokifreefire|quoteser|puisicinta|anggurmerah|bungamawar|anggrek|terangdalamgelap|terangdalamgelapku|baikbaik|baikbaiksayang|letdahyper|kopraljono|
+
+""".strip().split("|")
+ngntd=((())>(()))+((())>(()))
+def awok(ngentod):
+	return [ngentod,ngentod+"123",ngentod+"12345"]
+def pw_list(ngentot):
+	if len(ngentot)==2:
+		aap_afandi=ngentot[1].split(" ")
+		if len(aap_afandi[ngntd])!=ngntd:
+			asu=awok(aap_afandi[ngntd])
+			if len(aap_afandi[ngntd])<=5:
+				del asu[ngntd]
+				for x in pw_tambahan: asu.append(x)
+			else:
+				for x in pw_tambahan: asu.append(x)
+		else: asu=pw_tambahan
+	else: asu=pw_tambahan
+	return asu
